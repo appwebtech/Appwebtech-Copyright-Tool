63562e083a44203469e6cd339ff7402fdf878648
Joseph <josembi@gmail.com>
2017-12-08 16:39:47 +0100

    Initialise Repo & Generator sorta Scaffold

---
+++ lib/appwebtech_copyright_tool.rb
@@ -0,0 +1,5 @@
+require "appwebtech_copyright_tool/version"
+
+module AppwebtechCopyrightTool
+  # Your code goes here...
+end
---
+++ spec/appwebtech_copyright_tool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe AppwebtechCopyrightTool do
+  it "has a version number" do
+    expect(AppwebtechCopyrightTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
